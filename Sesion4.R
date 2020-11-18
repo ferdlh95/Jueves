31cf18109b1efded5788f61e79e3d531202cdf95
ferdlh95 <fernandodlh0795@gmail.com>
2020-11-18 02:20:02 +0000

    ok

---
+++ Sesion4.R
@@ -0,0 +1,10 @@
+### ARCHIVO DE PRUEBA PARA LA SESION 4. EJEMPLO 1
+
+vec<- c("Hola mundo")
+
+matriz2 <- data.frame(x= c(1,2,3,4),
+                      y= c(3,NA,2,6),
+                      z= c(1,2,NA,0),
+                      a= c(1,2,3,7)
+)
+complete.cases(matriz2)
